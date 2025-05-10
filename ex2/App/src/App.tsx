065b41bcdda5fe2dbd8109474f7e734ce78f990f
Rafael <rafarafa99944@gmail.com>
2025-05-10 19:23:36 -0300

    creating some elements

---
+++ ex2/App/src/App.tsx
@@ -0,0 +1,15 @@
+import './style/index.css'
+import Header from './components/Header'
+
+function App() {
+  
+  return (
+    <>
+      <Header/>
+      <h1 className="text-4xl">Hello, world!</h1>
+    </> 
+  )
+
+}
+
+export default App
