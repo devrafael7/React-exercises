c02e15988f0d9d380da9c84521d182894accb5f2
Rafael <rafarafa99944@gmail.com>
2025-05-13 00:32:12 -0300

    training userState and

--- ex2/App/src/App.tsx
+++ ex2/App/src/App.tsx
@@ -1,22 +1,22 @@
 import './style/index.css'
-<<<<<<< HEAD
-import Header from './components/Header'
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState("");
   
   return (
     <>
-      <Header/>
-      <h1 className="text-4xl">Hello, world!</h1>
+      <div className='p-40'>
+        <h1 className='text-4xl'>{count}</h1>
+        <button onClick={()=> setCount(count + 1)} type="button">
+          Set count
+        </button>
+        <input onChange={(e)=> setValue(e.target.value)} className='mx-10 border border-black border-solid rounded-sm px-3 py-2' placeholder='text' type="text" />
+        <h1 className='mx-5 text-xl'>{value}</h1>
+      </div>
     </> 
-  )
-
-=======
-import AppRoutes from './routes'
-
-function App() {
-  return <AppRoutes />
->>>>>>> a45a7c40a20145bf5906e8c1cbd1d2f138195422
+  ) 
 }
 
-export default App
+export default App;
--- ex2/App/src/main.tsx
+++ ex2/App/src/main.tsx
@@ -2,19 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './style/index.css'
 import App from './App.tsx'
-<<<<<<< HEAD
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-=======
-import { BrowserRouter } from 'react-router-dom'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
->>>>>>> a45a7c40a20145bf5906e8c1cbd1d2f138195422
   </StrictMode>,
 )
